628fd67b764ff8bca0749c675b621addec226e30
VydrOz <61025448+VydrOz@users.noreply.github.com>
2023-12-03 14:33:03 +0100

    :sparkles: Solve day 2

---
+++ AdventOfCode/AdventOfCode/2023/Day02/Solution.cs
@@ -0,0 +1,47 @@
+﻿namespace AdventOfCode.Y2023.Day02;
+
+using System.Text.RegularExpressions;
+using AdventOfCode.Solutions;
+
+class Solution : SolutionBase
+{
+    public Solution() : base(02, 2023, "Cube Conundrum") { }
+
+    protected override string SolvePartOne()
+    {
+        return Input.Select(ParseGame)
+            .Where(g => g.Red <= 12 && g.Green <= 13 && g.Blue <= 14)
+            .Sum(g => g.Id)
+            .ToString();
+    }
+
+    protected override string SolvePartTwo()
+    {
+        return Input.Select(ParseGame)
+            .Sum(g => g.Red * g.Green * g.Blue)
+            .ToString();
+    }
+
+    Game ParseGame(string line)
+    {
+        return new Game
+        {
+            Id = GetMatchedValue(line, @"Game (\d+)").First(),
+            Red = GetMatchedValue(line, @"(\d+) red").Max(),
+            Green = GetMatchedValue(line, @"(\d+) green").Max(),
+            Blue = GetMatchedValue(line, @"(\d+) blue").Max(),
+        };
+    }
+
+    IEnumerable<int> GetMatchedValue(string str, string regex)
+        => Regex.Matches(str, regex)
+            .Select(m => int.Parse(m.Groups[1].Value));
+}
+
+record Game
+{
+    public int Id;
+    public int Red;
+    public int Green;
+    public int Blue;
+}
