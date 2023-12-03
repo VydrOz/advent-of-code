c0929404d08a52fcb1b66162c1416e90cfc6fa00
VydrOz <61025448+VydrOz@users.noreply.github.com>
2023-12-03 16:39:12 +0100

    :construction: Solving day 3

---
+++ AdventOfCode/AdventOfCode/2023/Day03/Solution.cs
@@ -0,0 +1,40 @@
+﻿namespace AdventOfCode.Y2023.Day03;
+
+using System.Linq;
+using System.Text.RegularExpressions;
+using AdventOfCode.Solutions;
+
+class Solution : SolutionBase
+{
+    public Solution() : base(03, 2023, "Gear Ratios", true) { }
+
+    protected override string SolvePartOne()
+    {   
+        var lines = Input.ToList();
+        var specialChars = @"*$#+@-=/\";
+        var result = 0;
+        for (int i = 0; i < lines.Count; i++)
+        {
+            var line = lines[i];
+            var matches = Regex.Matches(line, @".(\d+).").Select(m => m);
+            foreach (var match in matches)
+            {
+                var index = line.IndexOf(match.Value, 0);
+                var before = lines.ElementAtOrDefault(i - 1)?[index..(index + match.Length)] ?? "";
+                var after = lines.ElementAtOrDefault(i + 1)?[index..(index + match.Length)] ?? "";
+
+                if(match.Value.Any(specialChars.Contains) || before.Any(specialChars.Contains) || after.Any(specialChars.Contains))
+                {
+                    result += int.Parse(Regex.Match(match.Value, @"(\d+)").Value);
+                }
+            }
+        }
+
+        return result.ToString();
+    }
+
+    protected override string SolvePartTwo()
+    {
+        return "";
+    }
+}
