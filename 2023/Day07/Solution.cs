1ef18b9861f4968f8ae5bc44335692a9d4b9ad23
VydrOz <61025448+VydrOz@users.noreply.github.com>
2023-12-11 19:27:20 +0100

    :construction: wip solve day 7 part one

--- 2023/Day07/Solution.cs
+++ 2023/Day07/Solution.cs
@@ -1,25 +1,19 @@
 namespace AdventOfCode.Y2023.Day07;
 
+using AdventOfCode.Y2023.Day04;
 using Lib;
 using System.Linq;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(07, 2023, "Camel Cards", true) { }
+    public Solution() : base(07, 2023, "Camel Cards") { }
 
     protected override string SolvePartOne()
     {
-        var hands = GetHands()
+        return GetHands()
             .OrderBy(GetHandType)
-            .ThenByDescending(GetHandPower);
-
-
-        foreach (var h in hands)
-        {
-            Console.WriteLine(h.cards);
-        }
-
-        return hands.Select((h, i) => h.bid * (i + 1))
+            .ThenBy(h => GetHandPower(h, "_23456789TJQKA"))
+            .Select((h, i) => h.bid * (i + 1))
             .Sum()
             .ToString();
     }
@@ -36,44 +30,18 @@ class Solution : SolutionBase
             .Select(l =>
                 new Hand(
                     cards: l[0],
-                    bid: int.Parse(l[1]) 
+                    bid: long.Parse(l[1]) 
             ));
     }
 
-    int GetHandType(Hand hand)
-    {
-        return int.Parse(hand.cards
-            .Select(c => hand.cards.Count(cc => cc == c))
-            .Order()
-            .Aggregate("", (c,n) => c + n));
-    }
+    long GetHandType(Hand hand)
+        => Parse(hand.cards.Select(c => hand.cards.Count(cc => cc == c)).OrderDescending());
 
-    long GetHandPower(Hand hand)
-    {
-        return long.Parse(hand.cards.Select(c => "23456789TJQKA".IndexOf(c)).Aggregate("", (c, n) => c + n));
-    }
+    long GetHandPower(Hand hand, string cards)
+        => Parse(hand.cards.Select(c => cards.IndexOf(c).ToString("00")));
+
+    long Parse<T>(IEnumerable<T> list)
+        => long.Parse(list.Aggregate("", (c,n) => c + n));
 }
 
 record Hand(string cards, long bid);
-
-//public class HandComparer : IComparer<string>
-//{
-//    private const string Cards = "23456789TJQKA";
-
-//#pragma warning disable CS8767 // Nullability of reference types in type of parameter doesn't match implicitly implemented member (possibly because of nullability attributes).
-//    public int Compare(string x, string y)
-//#pragma warning restore CS8767 // Nullability of reference types in type of parameter doesn't match implicitly implemented member (possibly because of nullability attributes).
-//    {
-//        for (int i = 0; i < 6; i++)
-//        {
-//            var iOne = Cards.IndexOf(x[i]);
-//            var iTwo = Cards.IndexOf(y[i]);
-
-//            var compare = iOne.CompareTo(iTwo);
-
-//            if (compare != 0) return compare;
-//        }
-
-//        return 0;
-//    }
-}
